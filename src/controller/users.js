2f29e46b3459caacc9e3fa41305f7ce7496f8ab8
yaminmuhammad <1910631170108@student.unsika.ac.id>
2022-11-17 19:16:41 +0700

    feat: update and delete users

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,6 +1,14 @@
 const getAllUsers = (req, res) => {
+  const data = {
+    id: "1",
+    name: "John Doe",
+    email: "JohnDoe3@gmail.com",
+    address: "Jl. Jalan",
+  };
+
   res.json({
     message: "GET users suksess!",
+    data: data,
   });
 };
 
@@ -12,7 +20,33 @@ const createUser = (req, res) => {
   });
 };
 
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  console.log("idUser:", id);
+
+  res.json({
+    message: "UPDATE users suksess!",
+    data: req.body,
+  });
+};
+
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+  console.log("idUser:", id);
+
+  res.json({
+    message: "DELETE users suksess!",
+    data: {
+      id: id,
+      email: "JohnDoe3@gmail.com",
+      address: "Jl. Jalan",
+    },
+  });
+};
+
 module.exports = {
   getAllUsers,
   createUser,
+  updateUser,
+  deleteUser,
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,13 @@ const router = express.Router();
 // CREATE POST
 router.post("/", userController.createUser);
 
+// READ GET
 router.get("/", userController.getAllUsers);
+
+// UPDATE - PATCH
+router.patch("/:id", userController.updateUser);
+
+// DELETE - DELETE
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
